908addf60b8910aa14fac70dfbb2f32ce53cf693
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-07-30 03:49:04 +1000

    added locations to schools

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=40)
+    lat = models.FloatField()
+    lng = models.FloatField()
 
     def __str__(self):
         return self.name
