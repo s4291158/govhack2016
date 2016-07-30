f76988149f97bf99f7ba98b7e4de300790839ac2
callumjhays <callumjhays@gmail.com>
2016-07-30 21:25:26 +1000

    codepen code

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
-document.onreadystatechange = () => {
-  var BASE_URL = 'http://qschools.online';
+$(() => {
+  let BASE_URL = 'http://qschools.online';
   
-  var map;
-  var markers = [];
-  var savedSchools = [];
-  var currentSchool = 0;
+  let map;
+  let markers = [];
+  let savedSchools = [];
+  let currentSchool = 0;
   
-  var locationsRecieved = $.getJSON(BASE_URL + '/school_locations/?format=json');
-  var mapInitialized = new Promise((resolve, reject) => {
+  let locationsRecieved = $.getJSON(BASE_URL + '/school_locations/?format=json');
+  let mapInitialized = new Promise((resolve, reject) => {
     window.initMap = function() {
-      var mapDiv = $('#map')[0];
+      let mapDiv = $('#map')[0];
       map = new google.maps.Map(mapDiv, {
         center: {lat: -27.4698, lng: 153.0251},
         zoom: 8
@@ -17,58 +17,35 @@ document.onreadystatechange = () => {
       resolve();
     }
   });
-
-  function openInfoWindow(id) {
-    console.log('ID: ', id);
-    $('#info').removeClass('closed');
-
-    $.post(BASE_URL + '/school', (school) => {
-      currentSchool = school;
-      console.log(school);
-    });
-  }
-
-  function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
+  
+  function generateMarkers(data) {
+    for(let i in data){
+      let marker = new google.maps.Marker({
+        position: {
+          lat: data[i].lat,
+          lng: data[i].lng
+        },
+        map: map,
+        title: ("" + data[i].name)
+      });
+      marker.addListener('click', (e) => {
+        openInfoWindow(data[i].id);
+      })
+      markers.push(marker);
     }
   }
-
-  function deleteAllMarkers(){
-    setMapOnAll(null);
-    markers = [];
-  }
-
-  generateMarkers = (data) => {
-    for(var i in data){
-        locations = data;
-        marker = new google.maps.Marker({
-          position: {
-            lat: locations[i].lat,
-            lng: locations[i].lng
-          },
-          map: map,
-          title: ("" + locations[i].name)
-        });
-        marker.addListener('click', (e) => {
-            console.log('ID: ', locations[i].id);
-            openInfoWindow(locations[i].id);
-        });
-    markers.push(marker);
-  }};
   
   // promise to handle all setup
   Promise
     .all([locationsRecieved, mapInitialized])
     .then((data) => {
-        var locations = data[0];
-        generateMarkers(locations);
-    }).catch((err) => { console.log("Error: " + err); });
+      generateMarkers(data[0]);
+    }).catch((err) => { console.log("Error: ", err); });
   
   // save for comparison button click handler
   $("#saveSchool").click((e) => {
     // if school already on list
-    var indxOf = savedSchools.indexOf(currentSchool);
+    let indxOf = savedSchools.indexOf(currentSchool);
     if(indxOf != -1){
       // remove it
       savedSchools.splice(indxOf, 1);
@@ -77,24 +54,45 @@ document.onreadystatechange = () => {
       $(e.currentTarget).find('i').addClass('fa-check-square-o').removeClass('fa-square-o');
     } else {
       // update graphics
-      $(e.currentTarget).find('i').addClass('fa-check-o').removeClass('fa-check-square-o');
+      $(e.currentTarget).find('i').addClass('fa-square-o').removeClass('fa-check-square-o');
 
       // update data
       savedSchools.push(currentSchool);
     }
   });
-
-  // on query search
-  $("#searchButton").click((e) => {
-    queryText = $('#query').text();
-    deleteAllMarkers();
-    $.post('/', {"query": queryText}, (response) => {
-        var locations = data[0];
-        try{
-            generateMarkers(locations);
-        }catch(err){
-        console.log(err);
-        }
-    })
+  
+  $("#info").click((e) => {
+    $("#info").toggleClass("offscreen");
   });
-};
+  
+  function clearAllMarkers() {
+    for(let i in markers){
+      markers[i].setMap(null);
+    }
+    markers = [];
+  }
+  
+  function handleSearch() {
+    let query = $("#srch-term").val();
+    console.log(query);
+    $.post(BASE_URL, {"query": query}, (data) => {
+      console.log(data);
+      clearAllMarkers();
+      generateMarkers(data);
+    });
+  };
+  
+  $("#srch-term").keyup((e) => {
+    if(e.keyCode == 13)
+      handleSearch;
+  });
+  $("#searchButton").click(handleSearch);
+  
+  function openInfoWindow(id) {
+    $.getJSON(BASE_URL + '/school/' + id,
+              (school) => {
+      currentSchool = school;
+      $("#info").removeClass("offscreen").text(JSON.stringify(school));
+    })
+  }
+});
