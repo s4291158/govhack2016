d0b9ea0fae3c71a6372807a8994c57fedaf135d0
Kendrick Tan <kendricktan0814@gmail.com>
2016-07-30 12:31:15 +1000

    revert

--- app/views.py
+++ app/views.py
@@ -4,15 +4,16 @@ from rest_framework.response import Response
 from app.models import School, schools_within_bounds
 from app.serializers import SchoolLocationsSerializer, BoundsSerializer
 
-from app.gmaps import query_place
 
 class SchoolLocationsView(APIView):
     def get(self, request):
-        school = School.objects.order_by('?').first()
-        return Response(data=query_place(school.postcode))
+        school_set = School.objects.all()
+        serializer = SchoolLocationsSerializer(school_set, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
-        # todo: pass through jaimyn's wit.ai thingy
-
-        # for now assume its a location
-        return Response(data=query_place(request.data['location']))
+        input_serializer = BoundsSerializer(data=request.data)
+        if input_serializer.is_valid(raise_exception=True):
+            school_set = schools_within_bounds(input_serializer.validated_data)
+            serializer = SchoolLocationsSerializer(school_set, many=True)
+            return Response(serializer.data)
