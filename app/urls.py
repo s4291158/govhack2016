c5804038bd05b7cb4500fd580aafbd0814f6213b
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-07-30 17:29:16 +1000

    started on SchoolSerializer

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from app.views import SchoolLocationsView, index_view
+from app.views import SchoolLocationsView, SchoolView, index_view
 
 urlpatterns = [
     url(r'^$', index_view, name='index_view'),
     url(r'^school_locations/$', SchoolLocationsView.as_view(), name='school_locations'),
+    url(r'^school/(?P<school_id>[0-9]+)/$', SchoolView.as_view(), name='school'),
 ]
