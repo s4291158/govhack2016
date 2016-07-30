face946541aee2f64a26f90f79e3691d77abc7d6
Kendrick Tan <kendricktan0814@gmail.com>
2016-07-31 09:55:01 +1000

    fixed attendance

--- app/views.py
+++ app/views.py
@@ -14,8 +14,6 @@ from app.serializers import SchoolLocationsSerializer, BoundsSerializer, \
 
 class MainView(APIView):
     def post(self, request):
-        # TODO: Pass string through Jaimyn's thing
-
         # Filter by location
         # location = query_place(location)
         # lat1 = lat - 0.05
@@ -27,36 +25,68 @@ class MainView(APIView):
         # Filter by
 
         # {'school': 'school', 'location': 'brisbane city', 'area': 'numeracy', 'attendance': 'good', 'suspension': 'short'}
-        query_data = request.POST['query']
+        query_data = request.data['query']
         keywords = get_the_query(query_data)
 
         queryset = School.objects.all()
 
+        valid_school_ids = [s.id for s in School.objects.all()]
+
         if 'location' in keywords:
             # Filter by location
             lat_lng, lat_lng_bounds = query_place(keywords['location'])
-            formatted_bounds = {'lng1': lat_lng_bounds['southwest']['lng']-0.01, 'lng2': lat_lng_bounds['northeast']['lng']+0.01,
-                                'lat1': lat_lng_bounds['southwest']['lat']-0.01, 'lat2': lat_lng_bounds['northeast']['lat']+0.01}
+            formatted_bounds = {'lng1': lat_lng_bounds['southwest']['lng'] - 0.01,
+                                'lng2': lat_lng_bounds['northeast']['lng'] + 0.01,
+                                'lat1': lat_lng_bounds['southwest']['lat'] - 0.01,
+                                'lat2': lat_lng_bounds['northeast']['lat'] + 0.01}
 
             bounds_serializer = BoundsSerializer(data=formatted_bounds)
             bounds_serializer.is_valid(raise_exception=True)
+
             queryset = schools_within_bounds(bounds_serializer.validated_data)
 
-        if 'attendence' in keywords:
-            # filter attendence
-            school_ids = []
+            temp_school_id = []
             for q in queryset:
-                # If they have a high attendence rate
-                if 'good' in keywords['attendence']:
-                    if len(q.attendence_set.all().filter(attendence_rate__gte=90.0)) > 0:
-                        school_ids.append(q.id)
-                else:
-                    if len(q.attendence_set.all().filter(attendence_rate__lte=89.0)) > 0:
-                        school_ids.append(q.id)
-            queryset.objects.filter(id__in=school_ids)
+                temp_school_id.append(q.id)
 
+            valid_school_ids = temp_school_id
+
+        if 'attendance' in keywords:
+            # filter attendence
+
+            temp_school_id = []
+
+            for i in valid_school_ids:
+                current_school = School.objects.get(id=i)
+
+                if 'good' in keywords['attendance']:
+                    # If they want good and this school aint good enough, pop
+                    if len(current_school.attendence_set.all().filter(attendence_rate__gte=90.0)) <= 0:
+                        temp_school_id.append(i)
+
+                elif 'high' in keywords['attendance']:
+                    if len(current_school.attendence_set.all().filter(attendence_rate__gte=95.0)) <= 0:
+                        temp_school_id.append(i)
+
+                # Assume
+                elif 'bad' in keywords['attendance']:
+                    if len(current_school.attendence_set.all().filter(attendence_rate__lte=85.0)) <= 0:
+                        temp_school_id.append(i)
+
+                # Assume average
+                else:
+                    if len(current_school.attendence_set.all().filter(attendence_rate__lte=100.0)) <= 0:
+                        temp_school_id.append(i)
+
+            valid_school_ids = temp_school_id
+
+        if 'area' in keywords:
+            print('reached')
+            print(keywords['area'])
+
+        queryset = School.objects.filter(id__in=valid_school_ids)
         serializer = SchoolLocationsSerializer(queryset, many=True)
-        return Response(data=serializer.data)#, headers={"Access-Control-Allow-Origin": '*'})
+        return Response(data=serializer.data)  # , headers={"Access-Control-Allow-Origin": '*'})
 
     def get(self, request):
         return render(request, 'index.html')
